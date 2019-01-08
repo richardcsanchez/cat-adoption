9c0bf2ad57379a2306c561143271c66d3dd5ec0a
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-08 20:55:13 +0000

    update ruby version

--- db/migrate/20190107231625_create_users.rb
+++ db/migrate/20190107231625_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :email
-      t.password_digest :password
+      t.string :password_digest
       t.string :name
       t.integer :money
       t.boolean :admin
---
+++ db/schema.rb
@@ -0,0 +1,47 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_01_08_192636) do
+
+  create_table "agencies", force: :cascade do |t|
+    t.string "name"
+    t.string "street_1"
+    t.string "street_2"
+    t.string "state"
+    t.integer "zip_code"
+    t.string "email"
+    t.integer "phone_number"
+  end
+
+  create_table "cats", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+    t.integer "age"
+    t.string "sex"
+    t.string "disposition"
+    t.integer "cost"
+    t.boolean "adopted"
+    t.integer "owner_id"
+    t.boolean "neutered"
+    t.string "image"
+    t.integer "agency_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "name"
+    t.integer "money"
+    t.boolean "admin"
+  end
+
+end
