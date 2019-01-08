4e71fac4586a7fb37b2175d196c1fcd83272df7e
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-08 18:59:33 +0000

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :name, presence: true
+  validates :money, presence: true
+  validates :admin, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
+
+
+  has_many :cats
+  has_many :agencies, through: :cats
 
 end
