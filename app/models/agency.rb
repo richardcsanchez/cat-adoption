ff81e7af166a5b359b41bd16fde91d6f13020b6a
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-08 21:04:48 +0000

    add agency validations

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,3 +1,9 @@
 class Agency < ApplicationRecord
+  validates :name, :state, :email, presence: :true
+  validates :phone_number, length: {is: 10}
 
+
+  include EmailValidatable
+  validates :email, uniqueness: true
+  validates :email, email: true
 end
