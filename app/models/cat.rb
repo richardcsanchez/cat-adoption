12702afebf4e55038cae6c7d7ecb84abc2ed0d5d
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-08 19:23:54 +0000

    add cat model validations

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,12 @@
 class Cat < ApplicationRecord
+  belongs_to :agency
+  belongs_to :owner
+
+  validates :name, :breed, :age, :sex, :adopted, :neutered, :agency_id, presence: :true
+  validates :owner_id, presence: true, if: :adopted?
+
+  def adopted?
+    self.adopted == true
+  end
+
 end
