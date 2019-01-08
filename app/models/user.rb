f9ff6e0ad64948ba960146309ef2846aa5c0b044
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-08 20:56:35 +0000

    update user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :name, presence: true
-  validates :money, presence: true
-  validates :admin, presence: true
+  validates :name, :money, :admin, :email, presence: true
 
   include EmailValidatable
-  validates :email, presence: true
   validates :email, uniqueness: true
   validates :email, email: true
-
-
 
   has_many :cats
   has_many :agencies, through: :cats
