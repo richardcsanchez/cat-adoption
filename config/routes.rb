895c5799b61a01a2b72ce4463e0a97862e05cd6c
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-08 20:35:55 +0000

    update gem file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/signin", to: "sessions#new"
+  post "/signin", to: "sessions#create"
+  get "/signout", to: "sessions#destroy"
+
+  resources :cats
+  resources :agencies
+  resources :users
+
+  root "welcome#home"
 end
