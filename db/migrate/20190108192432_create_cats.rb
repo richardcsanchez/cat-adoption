a2b48d1e625fbeaa4dc4049c6f28a97ac132d27d
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-08 19:28:18 +0000

    add create cats & agencies migrations

---
+++ db/migrate/20190108192432_create_cats.rb
@@ -0,0 +1,17 @@
+class CreateCats < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cats do |t|
+      t.string :name
+      t.string :breed
+      t.integer :age
+      t.string :sex
+      t.string :disposition
+      t.integer :cost
+      t.boolean :adopted
+      t.integer :owner_id
+      t.boolean :neutered
+      t.string :image
+      t.integer :agency_id
+    end
+  end
+end
---
+++ db/migrate/20190108192636_create_agencies.rb
@@ -0,0 +1,13 @@
+class CreateAgencies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :agencies do |t|
+      t.string :name
+      t.string :street_1
+      t.string :street_2
+      t.string :state
+      t.integer :zip_code
+      t.string :email
+      t.integer :phone_number
+    end
+  end
+end
