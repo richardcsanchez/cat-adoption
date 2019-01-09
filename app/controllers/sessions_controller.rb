fa54dc6be1b50e4d3a97af94c9759dedf067c2a7
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-09 20:13:31 +0000

    debug sign in form & sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,15 @@ class SessionsController < ApplicationController
   skip_before_action :require_logged_in, only: [:new, :create]
 
   def new
-    @user = User.new
   end
 
   def create
-    user = User.find_by(email: params[:user_email])
-    binding.pry
-    if user && user.authenticate(params[:password])
-     session[:user_id] = user.id
-     redirect_to user_path(user)
-   else
-     redirect_to signin_path
-   end
+    user = User.find_by(email: params[:user][:email])
+    user = user.try(:authenticate, params[:user][:password])
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+    session[:user_id] = user.id
+    @user = user
+    redirect_to controller: 'welcome', action: 'home'
   end
 
   def destroy
