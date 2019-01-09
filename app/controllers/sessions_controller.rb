8ebd07e5f1959325b415aa490233c0aea03c8715
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-09 19:54:03 +0000

    build sign in form and route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:user_name])
+    user = User.find_by(email: params[:user_email])
+    binding.pry
     if user && user.authenticate(params[:password])
      session[:user_id] = user.id
      redirect_to user_path(user)
@@ -19,5 +20,5 @@ class SessionsController < ApplicationController
     session.delete :user_id
     redirect_to root_path
   end
-  
+
 end
