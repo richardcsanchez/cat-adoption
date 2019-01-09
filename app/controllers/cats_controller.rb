2e26ecc75999f95b711c5a5f3227673f90c76be7
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-09 21:39:56 +0000

    build cats controller

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,21 @@
 class CatsController < ApplicationController
+
+  def index
+    @cats = Cat.all
+  end
+
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+     if @cat.save
+       redirect_to cat_path(@cat)
+     else
+       render :new
+     end
+  end
+
+
 end
