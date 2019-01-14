b5f7ca1eeb9182bcd9a03a7018cb949763cf5464
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-14 20:33:23 +0000

    add routes to agency controller

--- app/controllers/agency_controller.rb
+++ app/controllers/agency_controller.rb
@@ -1,2 +1,25 @@
-class AgencyController < ApplicationController
+class AgenciesController < ApplicationController
+  def new
+    @agency = Agency.new
+  end
+
+  def create
+    @agency = Agency.new(agency_params)
+     if @agency.save
+       redirect_to agency_path(@agency)
+     else
+       render :new
+     end
+  end
+
+  def show
+    @agency = Agency.find_by_id(params[:id])
+  end
+
+  private
+  def agency_params
+   params.require(:agency).permit(:name, :street_1, :street_2, :zip_code, :state, :email, :phone_number)
+  end
+
+
 end
