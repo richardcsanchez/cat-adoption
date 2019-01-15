daca6bcf33457c42bd3d74f469e8b741babd21af
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-15 23:34:44 +0000

    create select box for agency in new cat form

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,7 +1,7 @@
 class Agency < ApplicationRecord
   has_many :cats
   has_many :users, through: :cats
-  
+
   validates :name, :state, :email, presence: :true
   validates :phone_number, length: {is: 10}
 
@@ -9,4 +9,8 @@ class Agency < ApplicationRecord
   include EmailValidatable
   validates :email, uniqueness: true
   validates :email, email: true
+
+  def agency_name
+    self.find(params[:id]).name
+  end
 end
