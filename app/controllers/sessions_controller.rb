e8d65e88677023af200acae52c25a45d80865ea7
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-15 17:00:22 +0000

    update sessions controller & bin/setup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,10 @@ class SessionsController < ApplicationController
   skip_before_action :require_logged_in, only: [:new, :create]
 
   def new
-    @user = User.find_by(params[:email])
+      @user = User.find_by(params[:email])
+    if @user == nil
+      redirect_to root_path
+  end
   end
 
   def create
