5c6d8175d58cba53306ff99ef6874c5367e866f6
Richard Sanchez <richard.charles.s@gmail.com>
2019-01-15 20:52:00 +0000

    redirect from home page if already logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,9 @@ class WelcomeController < ApplicationController
   skip_before_action :require_logged_in
 
   def home
+    if current_user != nil
+      redirect_to user_path(current_user)
+    end
   end
 
 end
